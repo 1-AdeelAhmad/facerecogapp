ff0b186eb0c4531774d1acce39e857159d5dd3ae
Adeel <adeel@wfgames.co.uk>
2020-02-12 22:15:26 +0000

    Updates

---
+++ precache-manifest.7f6912b886ae15c63d88e18dd82b1984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268a771aed63b4ac79d48c3ef48811b7",
+    "url": "/facerecogapp/index.html"
+  },
+  {
+    "revision": "60962ec5c769bd84772c",
+    "url": "/facerecogapp/static/css/main.a78d3ccf.chunk.css"
+  },
+  {
+    "revision": "6b88d17648a8cb95bb36",
+    "url": "/facerecogapp/static/js/2.234bef71.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/facerecogapp/static/js/2.234bef71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60962ec5c769bd84772c",
+    "url": "/facerecogapp/static/js/main.3d3976d7.chunk.js"
+  },
+  {
+    "revision": "29c4d728b36421166f60",
+    "url": "/facerecogapp/static/js/runtime-main.277dbcfb.js"
+  }
+]);
